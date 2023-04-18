5edbc80bee6f04bc090ae1c0a9d38a566d04826f
Sebastian Rider <sebastian.rieder@live.com>
2023-04-18 18:03:09 +0200

    Create the Question Array

---
+++ JS-Files/quiz-array.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        "question": "wie lange dauerte der 30-jährige Krieg?",
+        "answer_1": "27 Jahre",
+        "answer_2": "30 Jahre",
+        "answer_3": "76 Jahre",
+        "answer_4": "2 Jahre",
+        "right_answer": 2
+    },
+    {
+        "question": "Du hast eine gefüllte Streichholzschachtel und kommst in einen leeren, kalten Raum, in dem sich nur ein Kohleofen, eine Petroleumlampe und ein Kamin steht. Was zündest du zuerst an?",
+        "answer_1": "Der Kohleofen",
+        "answer_2": "Der Kamin",
+        "answer_3": "Das Streichholz",
+        "answer_4": "Die Petroleumlampe",
+        "right_answer": 3
+    },
+    {
+        "question": "Was antwortet in allen Sprachen? Was spricht ohne Mund? Was hört ohne Ohren?",
+        "answer_1": "Das Echo",
+        "answer_2": "Das Meer",
+        "answer_3": "Der Wald",
+        "answer_4": "Die Berge",
+        "right_answer": 1
+    },
+    {
+        "question": "Welcher ist der „rote Planet“ unseres Sonnensystems?",
+        "answer_1": "Merkur",
+        "answer_2": "Uranus",
+        "answer_3": "Venus",
+        "answer_4": "Mars",
+        "right_answer": 4
+    },
+    {
+        "question": "Aus wie vielen Kräuter ist Jägermeister gemacht?",
+        "answer_1": "85",
+        "answer_2": "67",
+        "answer_3": "56",
+        "answer_4": "103",
+        "right_answer": 3
+    }
+];
